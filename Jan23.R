547af6cb27beeb4abaa8ad2b2a9ad9b816ab5b26
Denali Stevens <denalistevens01@gmail.com>
2024-02-13 13:57:35 -0500

    Cleaning

--- Jan23.R
+++
@@ -1,7 +0,0 @@
-# Create a random order of student for presentations
-roster <- tibble("Sam", "Matt", "Kasey", "Denali", "Sierra", "Brianna", "Lupi", "Callie")
-library(tidyverse)
-
-roster2 = data.frame(name =c ('Sam', 'Matt', 'Kasey', 'Denali', 'Sierra', 'Brianna', 'Lupi', 'Callie'))
-
-roster2[sample(1:nrow(roster2)),]
